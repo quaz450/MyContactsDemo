c9591c710fd8821dfafce1439a67c57bcae56159
quaz450 <js769933@dal.ca>
2020-06-17 14:02:55 -0300

    intitial commit
    add db.json file for testing

---
+++ app/src/main/java/com/quaz/mycontactsdemo/Contact.java
@@ -0,0 +1,44 @@
+package com.quaz.mycontactsdemo;
+
+public class Contact {
+
+    private int id;
+    private String image;
+    private String fistName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+
+
+    public Contact(String image, String fistName, String lastName, String email, String phoneNumber) {
+        this.image = image;
+        this.fistName = fistName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getFistName() {
+        return fistName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
